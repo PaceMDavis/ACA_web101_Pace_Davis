49506ed037585b9f5fc37e52bc218e2718be4921
Pace Davis <pacemdavis@gmail.com>
2019-10-03 11:18:56 -0500

    bouncing ball-square

---
+++ src/animtate-tut/animations.js
@@ -0,0 +1,7 @@
+function openNav () {
+  //this is where you do stuff
+  //show me nav-items--mobile
+  let navMobile = document.querySelector('.nav-items--mobile')
+  navMobile.classList.toggle("showNav")
+  // console.log('navMobile:',navMobile)
+}
