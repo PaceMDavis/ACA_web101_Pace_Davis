2848aff5b33ee2e52b78d1f8e0e780dd3f632d8a
Pace Davis <pacemdavis@gmail.com>
2019-10-07 20:22:24 -0500

    working on project

---
+++ src/checkpoint2/Coffee-checkpoint.js
@@ -0,0 +1,7 @@
+function openNav() {
+  // this is where you do stuff
+  // show me nav-item--mobile
+  let navMobile = document.querySelector('.nav-items--mobile')
+  navMobile.classList.toggle('showNav')
+  // console.log('navMobile:', navMobile.classList)
+}
