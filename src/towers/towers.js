5a66504e8e6438aeeb87b41b3cf552a7707c75aa
Pace Davis <pacemdavis@gmail.com>
2019-10-11 16:26:58 -0500

    messing with towers of hanoi

--- src/towers/towers.js
+++ src/towers/towers.js
@@ -4,14 +4,38 @@ $(document).ready(function() {
   
   let $block = null;
   
-  $('[data-row]').click(function() {
-    if ($block) {
-      $(this).append($block);
-      $block = null;
-    } else {
-      // let children = $(this).children()
-      // console.log('children', children)
-      $block = $(this).children().last().detach();
-    }
-  })
+//   $('[data-row]').click(function() {
+//     if ($block) {
+//       $(this).append($block);
+//       $block = null;
+//     } else {
+//       let children = $(this).children()
+//       console.log('children', children)
+//       $block = $(this).children().last().detach();
+//     }
+//   })
+
+let ring = $('[data-color]')
+$(function() {
+  $(ring).draggable();
+  // $('[data-row]').click(function() {
+  //   if ($block) {
+  //     $(this).append($block);
+  //     $block = null;
+  //   } else {
+  //     let children = $(this).children()
+  //     console.log('children', children)
+  //     $block = $(this).children().last().detach();
+  //   }
+  // })
+});
+
+
+//three squares - different sizes
+//larger squares cannot go on top of smaller
+//pick up squares and drag them
+//drop them
+//if squares are smaller, let them go on top
+//if larger, return to original place
+//if all three reach the end, announce a
 })
