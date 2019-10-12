3fdc5d0c13a81ccbb351e3fa4c3abe7ab031c93b
Pace Davis <pacemdavis@gmail.com>
2019-10-11 21:47:28 -0500

    more tower changes. Pieces are moving

--- src/towers/towers.js
+++ src/towers/towers.js
@@ -14,23 +14,47 @@ $(document).ready(function() {
 //       $block = $(this).children().last().detach();
 //     }
 //   })
-
+let row = $('[data-row]')
 let ring = $('[data-color]')
+let dropRing = $('.red' || '.green' || '.blue')
+// $(function() {
+//   $(ring).draggable( {
+//   });
+ 
+//   // $('[data-row]').click(function() {
+//   //   if ($block) {
+//   //     $(this).append($block);
+//   //     $block = null;
+//   //   } else {
+//   //     let children = $(this).children()
+//   //     console.log('children', children)
+//   //     $block = $(this).children().last().detach();
+//   //   }
+//   // })
+// });
 $(function() {
-  $(ring).draggable();
-  // $('[data-row]').click(function() {
-  //   if ($block) {
-  //     $(this).append($block);
-  //     $block = null;
-  //   } else {
-  //     let children = $(this).children()
-  //     console.log('children', children)
-  //     $block = $(this).children().last().detach();
-  //   }
+  
+
+  $(ring).draggable( {
+    snap: row,
+    cursor: 'move',
+    // snapMode: "inner",
+    revert: 'invalid',
+    drop: function(droppedElement) {
+      let validDrop = droppedElement && droppedElement.dropRing;
+      return !validDrop;
+    },
+    
+  });
+  $(row).droppable( {
+    
+  });
+  // $(row).ondrop(function() {
+  //   if ring 
   // })
-});
+})
 
-
+//containment: '.red' || '.green' || '.blue',
 //three squares - different sizes
 //larger squares cannot go on top of smaller
 //pick up squares and drag them
